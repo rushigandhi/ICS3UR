fc84376f9195ce10a85c54815431d56b9673a5fb
Rushi Gandhi <rushigandhi@users.noreply.github.com>
2016-09-20 23:12:04 -0400

    Add files via upload

---
+++ J2A2P4.java
@@ -0,0 +1,7 @@
+public class J2A2P4
+{
+public static  void main(String[] args)
+{
+System.out.println("My new and improved\nJava Application\nBy: Rushi Gandhi");
+}
+}
---
+++ J2A3P1.java
@@ -0,0 +1,7 @@
+public class J2A3P1
+{
+public static  void main(String[] args)
+{
+System.out.println("   X\n  XXX\n XXXXX\nXXXXXXX\n   X");
+}
+}

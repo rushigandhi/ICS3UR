76875e6a3ceec75a59c9c0f06a00e68538e11541
Rushi Gandhi <rushigandhi@users.noreply.github.com>
2016-11-15 18:51:18 -0500

    Add files via upload

---
+++ J4A1.java
@@ -0,0 +1,46 @@
+// Assignment: J4A1
+// Description: Demonstrates Boolean Expressions
+//
+// Author: Rushi Gandhi
+//
+// Date Start: October 25, 2016
+// Date Completed: October 25, 2016
+//
+// Completion time: 2 periods
+//
+// Honor Code: I pledge that this program represents my own
+//   program code. I received help from no one in designing and debugging my program.
+//*********************************************************   
+
+   class DemonstrateBoolean{
+   public static void main(String[] args){
+   
+   // Step 1: Output
+   
+   System.out.println(" (-5 == 7) is " + (-5 == 7)); 
+   System.out.println(" (7 == 7) is " + (7 == 7));
+   System.out.println(" (5 <= 7) is " + (5 <= 7));
+   System.out.println(" (7 <= 5) is " + (7 <= 5));
+   System.out.println(" (-5 < 7) is " + (-5 < 7));
+   System.out.println(" (8 < 7) is " + (8 < 7));
+   System.out.println(" (-5 >= 7) is " + (-5 >= 7));
+   System.out.println(" (15 >= 7) is " + (15 >= 7));
+   System.out.println(" (15 > 7) is " + (15 > 7));
+   System.out.println(" (-5 > -7) is " + (-5 > -7));
+   System.out.println(" (-5 > 7) is " + (-5 > 7));
+   System.out.println(" (7 < 7) is " + (7 < 7));
+   System.out.println(" (-5 > -5) is " + (-5 > -5));
+   System.out.println(" (-5 != 7) is " + (-5 != 7));
+   System.out.println(" (-5 != -5) is " + (-5 != -5));
+   System.out.println(" (-5 != -(-5)) is " + (-5 != -(-5)));
+   System.out.println(" !(8 == 8) is " + !(8 == 8));
+   System.out.println(" !(8 == 9) is " + !(8 == 9));
+   System.out.println(" (\"hello\".equals(\"hello\")) is " + ("hello".equals("hello")));
+   System.out.println(" (\"hello\".equals(\"goodbye\")) is " + ("hello".equals("goodbye")));
+   System.out.println(" !(\"hello\".equals(\"goodbye\")) is " + !("hello".equals("goodbye")));
+   System.out.println(" !(\"goodbye\".equals(\"goodbye\")) is " + !("goodbye".equals("goodbye")));
+
+     }
+}
+   
+   
